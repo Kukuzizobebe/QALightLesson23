0d67edfe16c32e4b1f2414e2f3eac099cd23bd28
Rud Andrii <and22715@gmail.com>
2026-05-05 22:51:23 +0300

    Add auto tests again

---
+++ src/test/java/AQALesson2/CategoryExistInProductCatalogVerificationTest.java
@@ -0,0 +1,46 @@
+package AQALesson2;
+
+import junit.framework.Assert;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.util.List;
+
+import static java.lang.String.format;
+
+public class CategoryExistInProductCatalogVerificationTest {
+
+    static void main(String[] args) throws InterruptedException {
+
+        String category = "LEGO";
+        WebDriver driver = new ChromeDriver();
+
+        try {
+            driver.get("https://hotline.ua/");
+            driver.manage().window().maximize();
+
+            Thread.sleep(3000);
+
+            WebElement productCatalogButton = driver.findElement(By.xpath("//div[@class='button-menu-main ']"));
+            productCatalogButton.click();
+
+            Thread.sleep(3000);
+
+            List<WebElement> categoriesInProductCategoriesMenu = driver.findElements(By.xpath("//ul[@class='menu-main__list']//li"));
+
+            String errorMessage = "Category is incorrect. It doesn`t exist in product category menu";
+
+            boolean isCategoryPresent = categoriesInProductCategoriesMenu.stream()
+                    .map(WebElement::getText)
+                    .anyMatch(o -> o.contains(category));
+
+            Assert.assertTrue(errorMessage, isCategoryPresent);
+
+        } finally {
+            driver.quit();
+        }
+
+    }
+}
